29cd3d010ff058b52a253e405ae3a5d65d879301
Robert Metzger <rmetzger@apache.org>
2015-03-18 18:13:04 +0100

    wip

--- flink-staging/flink-streaming/flink-streaming-connectors/src/main/java/org/apache/flink/streaming/connectors/kafka/partitioner/SerializableKafkaPartitioner.java
+++ flink-staging/flink-streaming/flink-streaming-connectors/src/main/java/org/apache/flink/streaming/connectors/kafka/partitioner/SerializableKafkaPartitioner.java
@@ -14,12 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.flink.streaming.connectors.kafka.partitioner;
 
-
 import kafka.producer.Partitioner;
-
 import java.io.Serializable;
 
 public interface SerializableKafkaPartitioner extends Serializable, Partitioner {
