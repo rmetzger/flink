320972ced118926cf304d851b8f379e8dab56607
Gábor Hermann <reckoner42@gmail.com>
2015-03-18 13:19:02 +0100

    Fixes + cleanup

--- flink-staging/flink-streaming/flink-streaming-connectors/src/test/java/org/apache/flink/streaming/connectors/kafka/KafkaITCase.java
+++ flink-staging/flink-streaming/flink-streaming-connectors/src/test/java/org/apache/flink/streaming/connectors/kafka/KafkaITCase.java
@@ -24,11 +24,9 @@ import java.net.UnknownHostException;
 import java.util.BitSet;
 import java.util.Properties;
 
-import kafka.server.KafkaConfig;
-import kafka.server.KafkaServer;
-import kafka.utils.Time;
 import org.apache.curator.test.TestingServer;
 import org.apache.flink.runtime.client.JobExecutionException;
+import org.apache.flink.runtime.net.NetUtils;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -37,6 +35,7 @@ import org.apache.flink.streaming.api.function.source.SourceFunction;
 import org.apache.flink.streaming.connectors.kafka.api.KafkaSink;
 import org.apache.flink.streaming.connectors.kafka.api.simple.KafkaTopicUtils;
 import org.apache.flink.streaming.connectors.kafka.api.simple.PersistentKafkaSource;
+import org.apache.flink.streaming.connectors.kafka.api.simple.offset.Offset;
 import org.apache.flink.streaming.connectors.util.JavaDefaultStringSchema;
 import org.apache.flink.util.Collector;
 import org.junit.Assert;
@@ -47,10 +46,14 @@ import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServer;
+import kafka.utils.Time;
+
 /**
  * Code in this test is based on the following GitHub repository:
  * (as per commit bc6b2b2d5f6424d5f377aa6c0871e82a956462ef)
- *
+ * <p/>
  * https://github.com/sakserv/hadoop-mini-clusters (ASL licensed)
  */
 
@@ -60,24 +63,26 @@ public class KafkaITCase {
 
 	private final String TOPIC = "myTopic";
 
-	private final int ZK_PORT = 6667;
-	private final int KAFKA_PORT = 6668;
+	private int zkPort;
+	private int kafkaPort;
 	private String kafkaHost;
+
 	private String zookeeperConnectionString;
 
 	@Rule
 	public TemporaryFolder tempFolder = new TemporaryFolder();
-
 	public File tmpZkDir;
 	public File tmpKafkaDir;
+
 	@Before
 	public void prepare() throws IOException {
 		tmpZkDir = tempFolder.newFolder();
 		tmpKafkaDir = tempFolder.newFolder();
 		kafkaHost = InetAddress.getLocalHost().getHostName();
-		zookeeperConnectionString = "localhost:"+ZK_PORT;
+		zkPort = NetUtils.getAvailablePort();
+		kafkaPort = NetUtils.getAvailablePort();
+		zookeeperConnectionString = "localhost:" + zkPort;
 	}
-
 
 	@Test
 	public void test() {
@@ -94,7 +99,7 @@ public class KafkaITCase {
 			startKafkaTopology();
 
 			LOG.info("Test succeeded.");
-		} catch(Throwable t) {
+		} catch (Throwable t) {
 			LOG.warn("Test failed with exception", t);
 			Assert.fail("Test failed with: " + t.getMessage());
 		} finally {
@@ -106,7 +111,7 @@ public class KafkaITCase {
 				try {
 					zookeeper.stop();
 				} catch (IOException e) {
-					LOG.warn("ZK.stop() failed",e);
+					LOG.warn("ZK.stop() failed", e);
 				}
 			}
 		}
@@ -122,27 +127,29 @@ public class KafkaITCase {
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(1);
 
 		// add consuming topology:
-		DataStreamSource<String> consuming = env.addSource(new PersistentKafkaSource<String>(zookeeperConnectionString, TOPIC, new JavaDefaultStringSchema()));
+		DataStreamSource<String> consuming = env.addSource(
+				new PersistentKafkaSource<String>(zookeeperConnectionString, TOPIC, new JavaDefaultStringSchema(), 5000, 100, Offset.FROM_BEGINNING));
 		consuming.addSink(new SinkFunction<String>() {
 			int elCnt = 0;
 			int start = -1;
 			BitSet validator = new BitSet(101);
+
 			@Override
 			public void invoke(String value) throws Exception {
-				LOG.info("Got "+value);
+				LOG.info("Got " + value);
 				String[] sp = value.split("-");
 				int v = Integer.parseInt(sp[1]);
-				if(start == -1) {
+				if (start == -1) {
 					start = v;
 				}
 				Assert.assertFalse("Received tuple twice", validator.get(v - start));
 				validator.set(v - start);
 				elCnt++;
-				if(elCnt == 100) {
+				if (elCnt == 100) {
 					// check if everything in the bitset is set to true
 					int nc;
-					if((nc = validator.nextClearBit(0)) != 100) {
-						throw new RuntimeException("The bitset was not set to 1 on all elements. Next clear:"+nc+" Set: "+validator);
+					if ((nc = validator.nextClearBit(0)) != 100) {
+						throw new RuntimeException("The bitset was not set to 1 on all elements. Next clear:" + nc + " Set: " + validator);
 					}
 					throw new SuccessException();
 				}
@@ -156,15 +163,17 @@ public class KafkaITCase {
 		// add producing topology
 		DataStream<String> stream = env.addSource(new SourceFunction<String>() {
 			boolean running = true;
+
 			@Override
 			public void run(Collector<String> collector) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
-				while(running) {
+				while (running) {
 					collector.collect("kafka-" + cnt++);
 					try {
 						Thread.sleep(100);
-					} catch(InterruptedException ignored) {}
+					} catch (InterruptedException ignored) {
+					}
 				}
 			}
 
@@ -179,12 +188,12 @@ public class KafkaITCase {
 		try {
 			env.setDegreeOfParallelism(1);
 			env.execute();
-		} catch(JobExecutionException good) {
+		} catch (JobExecutionException good) {
 			Throwable t = good.getCause();
 			int limit = 0;
-			while( !(t instanceof SuccessException)) {
+			while (!(t instanceof SuccessException)) {
 				t = t.getCause();
-				if(limit++ == 20) {
+				if (limit++ == 20) {
 					throw good;
 				}
 			}
@@ -193,7 +202,7 @@ public class KafkaITCase {
 
 
 	private TestingServer getZookeeper() throws Exception {
-		return new TestingServer(ZK_PORT, tmpZkDir);
+		return new TestingServer(zkPort, tmpZkDir);
 	}
 
 	/**
@@ -203,7 +212,7 @@ public class KafkaITCase {
 		Properties kafkaProperties = new Properties();
 		// properties have to be Strings
 		kafkaProperties.put("advertised.host.name", kafkaHost);
-		kafkaProperties.put("port", Integer.toString(KAFKA_PORT));
+		kafkaProperties.put("port", Integer.toString(kafkaPort));
 		kafkaProperties.put("broker.id", Integer.toString(brokerId));
 		kafkaProperties.put("log.dir", tmpKafkaDir.toString());
 		kafkaProperties.put("zookeeper.connect", zookeeperConnectionString);
