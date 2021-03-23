flink-ci/flink-mirror

[FLINK-21844][runtime] Do not auto-configure maxParallelism in REACTIVE scheduling mode
In Reactive scheduling mode, automatically setting
the max parallelism of vertices if unconfigured
leads to poor user experience: when switching
between modes, savepoint compatibility would break;
max parallelism is not something that is naturally
thought in the context of schedulers.

This change consolidates configuring parallelism
settings for both reactive mode and the default mode,
as well as makes the JobVertex the source of truth
for what's been configured.

Signed-off-by: austin ce <austin.cawley@gmail.com>

flink-runtime/src/test/java/org/apache/flink/runtime/executiongraph/ExecutionGraphRestartTest.java
@@ -32,6 +32,7 @@ import org.apache.flink.runtime.executiongraph.failover.flip1.TestRestartBackoff
 import org.apache.flink.runtime.executiongraph.utils.SimpleAckingTaskManagerGateway;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.jobgraph.JobGraphBuilder;
+import org.apache.flink.runtime.jobgraph.JobGraphConfigurationUtils;
 import org.apache.flink.runtime.jobgraph.JobGraphTestUtils;
 import org.apache.flink.runtime.jobgraph.JobVertex;
 import org.apache.flink.runtime.jobmanager.slots.TaskManagerGateway;
@@ -50,7 +51,6 @@ import org.apache.flink.runtime.scheduler.SchedulerTestingUtils;
 import org.apache.flink.runtime.taskexecutor.slot.SlotOffer;
 import org.apache.flink.runtime.taskmanager.LocalTaskManagerLocation;
 import org.apache.flink.runtime.taskmanager.TaskManagerLocation;
-import org.apache.flink.runtime.testtasks.NoOpInvokable;
 import org.apache.flink.util.TestLogger;
 
 import org.junit.Before;
@@ -214,9 +214,8 @@ public class ExecutionGraphRestartTest extends TestLogger {
      */
     @Test
     public void testFailingExecutionAfterRestart() throws Exception {
-        JobVertex sender = ExecutionGraphTestUtils.createJobVertex("Task1", 1, NoOpInvokable.class);
-        JobVertex receiver =
-                ExecutionGraphTestUtils.createJobVertex("Task2", 1, NoOpInvokable.class);
+        JobVertex sender = ExecutionGraphTestUtils.createNoOpVertex("Task1", 1);
+        JobVertex receiver = ExecutionGraphTestUtils.createNoOpVertex("Task2", 1);
         JobGraph jobGraph = JobGraphTestUtils.streamingJobGraph(sender, receiver);
 
         try (SlotPool slotPool = createSlotPoolImpl()) {
@@ -359,21 +358,22 @@ public class ExecutionGraphRestartTest extends TestLogger {
     }
 
     private static JobGraph createJobGraph() {
-        JobVertex sender =
-                ExecutionGraphTestUtils.createJobVertex("Task", NUM_TASKS, NoOpInvokable.class);
+        JobVertex sender = ExecutionGraphTestUtils.createNoOpVertex("Task", NUM_TASKS);
         return JobGraphTestUtils.streamingJobGraph(sender);
     }
 
     private static JobGraph createJobGraphToCancel() throws IOException {
-        JobVertex vertex =
-                ExecutionGraphTestUtils.createJobVertex("Test Vertex", 1, NoOpInvokable.class);
+        JobVertex vertex = ExecutionGraphTestUtils.createNoOpVertex("Test Vertex", 1);
         ExecutionConfig executionConfig = new ExecutionConfig();
         executionConfig.setRestartStrategy(
                 RestartStrategies.fixedDelayRestart(Integer.MAX_VALUE, Integer.MAX_VALUE));
 
-        return JobGraphBuilder.newStreamingJobGraphBuilder()
-                .addJobVertex(vertex)
-                .setExecutionConfig(executionConfig)
-                .build();
+        JobGraph graph =
+                JobGraphBuilder.newStreamingJobGraphBuilder()
+                        .addJobVertex(vertex)
+                        .setExecutionConfig(executionConfig)
+                        .build();
+        JobGraphConfigurationUtils.configureJobGraphForDefaultMode(graph);
+        return graph;
     }
 }

flink-runtime/src/test/java/org/apache/flink/runtime/jobgraph/JobGraphConfigurationUtilsAutoConfigurtionTest.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.jobgraph;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.apache.flink.runtime.jobgraph.JobGraphTestUtils.createJobVertex;
+import static org.apache.flink.runtime.jobgraph.JobGraphTestUtils.streamingJobGraph;
+
+@RunWith(Parameterized.class)
+public class JobGraphConfigurationUtilsAutoConfigurtionTest {
+
+    @Parameterized.Parameters(name = "parallelism = {0}, maxParallelism = {1}, expected max = {2}")
+    public static Object[][] data() {
+        return new Object[][] {
+            // default minimum
+            {1, JobVertex.MAX_PARALLELISM_DEFAULT, 128},
+            // test round up part 1
+            {171, JobVertex.MAX_PARALLELISM_DEFAULT, 256},
+            // test round up part 2
+            {172, JobVertex.MAX_PARALLELISM_DEFAULT, 512},
+            // test round up limit
+            {1 << 15, JobVertex.MAX_PARALLELISM_DEFAULT, 1 << 15},
+            // test configured / trumps computed default
+            {4, 1 << 15, 1 << 15},
+            // test override trumps test configured 2
+            {4, 7, 7},
+        };
+    }
+
+    @Parameterized.Parameter(0)
+    public int parallelism;
+
+    @Parameterized.Parameter(1)
+    public int maxParallelism;
+
+    @Parameterized.Parameter(2)
+    public int expectedMaxParallelism;
+
+    @Test
+    public void testMaxParallelismDefaulting() {
+        JobVertex jobVertex = createJobVertex(parallelism, maxParallelism);
+        JobGraph jobGraph = streamingJobGraph(jobVertex);
+        JobGraphConfigurationUtils.configureJobGraphForDefaultMode(jobGraph);
+        Assert.assertEquals(expectedMaxParallelism, jobVertex.getMaxParallelism());
+    }
+
+    @Test
+    public void testMaxParallelismDefaultingReactiveMode() {
+        JobVertex jobVertex = createJobVertex(parallelism, maxParallelism);
+        JobGraph jobGraph = streamingJobGraph(jobVertex);
+        JobGraphConfigurationUtils.configureJobGraphForReactiveMode(jobGraph);
+        Assert.assertEquals(expectedMaxParallelism, jobVertex.getMaxParallelism());
+        Assert.assertEquals(expectedMaxParallelism, jobVertex.getParallelism());
+    }
+}

flink-runtime/src/test/java/org/apache/flink/runtime/jobgraph/JobGraphTestUtils.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.runtime.jobgraph;
 
+import org.apache.flink.runtime.jobgraph.tasks.AbstractInvokable;
 import org.apache.flink.runtime.testtasks.NoOpInvokable;
 
 import java.util.Arrays;
@@ -30,22 +31,45 @@ public class JobGraphTestUtils {
     }
 
     public static JobGraph singleNoOpJobGraph() {
-        JobVertex jobVertex = new JobVertex("jobVertex");
+        JobVertex jobVertex = createJobVertex("jobVertex", 1, JobVertex.MAX_PARALLELISM_DEFAULT);
         jobVertex.setInvokableClass(NoOpInvokable.class);
 
-        return JobGraphBuilder.newStreamingJobGraphBuilder().addJobVertex(jobVertex).build();
+        return streamingJobGraph(jobVertex);
     }
 
     public static JobGraph streamingJobGraph(JobVertex... jobVertices) {
-        return JobGraphBuilder.newStreamingJobGraphBuilder()
-                .addJobVertices(Arrays.asList(jobVertices))
-                .build();
+        JobGraph graph =
+                JobGraphBuilder.newStreamingJobGraphBuilder()
+                        .addJobVertices(Arrays.asList(jobVertices))
+                        .build();
+        JobGraphConfigurationUtils.configureJobGraphForDefaultMode(graph);
+        return graph;
     }
 
     public static JobGraph batchJobGraph(JobVertex... jobVertices) {
-        return JobGraphBuilder.newBatchJobGraphBuilder()
-                .addJobVertices(Arrays.asList(jobVertices))
-                .build();
+        JobGraph graph =
+                JobGraphBuilder.newBatchJobGraphBuilder()
+                        .addJobVertices(Arrays.asList(jobVertices))
+                        .build();
+        JobGraphConfigurationUtils.configureJobGraphForDefaultMode(graph);
+        return graph;
+    }
+
+    public static JobVertex createJobVertex(int parallelism, int preconfiguredMaxParallelism) {
+        return createJobVertex("testVertex", parallelism, preconfiguredMaxParallelism);
+    }
+
+    public static JobVertex createJobVertex(
+            String name, int parallelism, int preconfiguredMaxParallelism) {
+        JobVertex jobVertex = new JobVertex(name);
+        jobVertex.setInvokableClass(AbstractInvokable.class);
+        jobVertex.setParallelism(parallelism);
+
+        if (JobVertex.MAX_PARALLELISM_DEFAULT != preconfiguredMaxParallelism) {
+            jobVertex.setMaxParallelism(preconfiguredMaxParallelism);
+        }
+
+        return jobVertex;
     }
 
     private JobGraphTestUtils() {

flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/TestUtils.java
@@ -25,6 +25,7 @@ import org.apache.flink.runtime.checkpoint.OperatorSubtaskState;
 import org.apache.flink.runtime.checkpoint.metadata.CheckpointMetadata;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.jobgraph.JobGraphBuilder;
+import org.apache.flink.runtime.jobgraph.JobGraphConfigurationUtils;
 import org.apache.flink.runtime.jobgraph.JobVertex;
 import org.apache.flink.runtime.jobgraph.OperatorID;
 import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
@@ -109,11 +110,15 @@ public class TestUtils {
         final JobCheckpointingSettings checkpointingSettings =
                 new JobCheckpointingSettings(checkpoinCoordinatorConfiguration, null);
 
-        return JobGraphBuilder.newStreamingJobGraphBuilder()
-                .addJobVertices(Arrays.asList(jobVertices))
-                .setJobCheckpointingSettings(checkpointingSettings)
-                .setSavepointRestoreSettings(savepointRestoreSettings)
-                .build();
+        JobGraph graph =
+                JobGraphBuilder.newStreamingJobGraphBuilder()
+                        .addJobVertices(Arrays.asList(jobVertices))
+                        .setJobCheckpointingSettings(checkpointingSettings)
+                        .setSavepointRestoreSettings(savepointRestoreSettings)
+                        .build();
+
+        JobGraphConfigurationUtils.configureJobGraphForDefaultMode(graph);
+        return graph;
     }
 
     private TestUtils() {

rmetzger/flink

flink-runtime/src/test/java/org/apache/flink/runtime/executiongraph/ExecutionGraphRestartTest.java
@@ -32,6 +32,7 @@ import org.apache.flink.runtime.executiongraph.failover.flip1.TestRestartBackoff
 import org.apache.flink.runtime.executiongraph.utils.SimpleAckingTaskManagerGateway;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.jobgraph.JobGraphBuilder;
+import org.apache.flink.runtime.jobgraph.JobGraphConfigurationUtils;
 import org.apache.flink.runtime.jobgraph.JobGraphTestUtils;
 import org.apache.flink.runtime.jobgraph.JobVertex;
 import org.apache.flink.runtime.jobmanager.slots.TaskManagerGateway;
@@ -50,7 +51,6 @@ import org.apache.flink.runtime.scheduler.SchedulerTestingUtils;
 import org.apache.flink.runtime.taskexecutor.slot.SlotOffer;
 import org.apache.flink.runtime.taskmanager.LocalTaskManagerLocation;
 import org.apache.flink.runtime.taskmanager.TaskManagerLocation;
-import org.apache.flink.runtime.testtasks.NoOpInvokable;
 import org.apache.flink.util.TestLogger;
 
 import org.junit.Before;
@@ -214,9 +214,8 @@ public class ExecutionGraphRestartTest extends TestLogger {
      */
     @Test
     public void testFailingExecutionAfterRestart() throws Exception {
-        JobVertex sender = ExecutionGraphTestUtils.createJobVertex("Task1", 1, NoOpInvokable.class);
-        JobVertex receiver =
-                ExecutionGraphTestUtils.createJobVertex("Task2", 1, NoOpInvokable.class);
+        JobVertex sender = ExecutionGraphTestUtils.createNoOpVertex("Task1", 1);
+        JobVertex receiver = ExecutionGraphTestUtils.createNoOpVertex("Task2", 1);
         JobGraph jobGraph = JobGraphTestUtils.streamingJobGraph(sender, receiver);
 
         try (SlotPool slotPool = createSlotPoolImpl()) {
@@ -359,21 +358,22 @@ public class ExecutionGraphRestartTest extends TestLogger {
     }
 
     private static JobGraph createJobGraph() {
-        JobVertex sender =
-                ExecutionGraphTestUtils.createJobVertex("Task", NUM_TASKS, NoOpInvokable.class);
+        JobVertex sender = ExecutionGraphTestUtils.createNoOpVertex("Task", NUM_TASKS);
         return JobGraphTestUtils.streamingJobGraph(sender);
     }
 
     private static JobGraph createJobGraphToCancel() throws IOException {
-        JobVertex vertex =
-                ExecutionGraphTestUtils.createJobVertex("Test Vertex", 1, NoOpInvokable.class);
+        JobVertex vertex = ExecutionGraphTestUtils.createNoOpVertex("Test Vertex", 1);
         ExecutionConfig executionConfig = new ExecutionConfig();
         executionConfig.setRestartStrategy(
                 RestartStrategies.fixedDelayRestart(Integer.MAX_VALUE, Integer.MAX_VALUE));
 
-        return JobGraphBuilder.newStreamingJobGraphBuilder()
-                .addJobVertex(vertex)
-                .setExecutionConfig(executionConfig)
-                .build();
+        JobGraph graph =
+                JobGraphBuilder.newStreamingJobGraphBuilder()
+                        .addJobVertex(vertex)
+                        .setExecutionConfig(executionConfig)
+                        .build();
+        JobGraphConfigurationUtils.autoConfigureMaxParallelism(graph);
+        return graph;
     }
 }

flink-runtime/src/test/java/org/apache/flink/runtime/jobgraph/JobGraphConfigurationUtilsAutoConfigurtionTest.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.jobgraph;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.apache.flink.runtime.jobgraph.JobGraphTestUtils.createJobVertex;
+import static org.apache.flink.runtime.jobgraph.JobGraphTestUtils.streamingJobGraph;
+
+@RunWith(Parameterized.class)
+public class JobGraphConfigurationUtilsAutoConfigurtionTest {
+
+    @Parameterized.Parameters(name = "parallelism = {0}, maxParallelism = {1}, expected max = {2}")
+    public static Object[][] data() {
+        return new Object[][] {
+            // default minimum
+            {1, JobVertex.MAX_PARALLELISM_DEFAULT, 128},
+            // test round up part 1
+            {171, JobVertex.MAX_PARALLELISM_DEFAULT, 256},
+            // test round up part 2
+            {172, JobVertex.MAX_PARALLELISM_DEFAULT, 512},
+            // test round up limit
+            {1 << 15, JobVertex.MAX_PARALLELISM_DEFAULT, 1 << 15},
+            // test configured / trumps computed default
+            {4, 1 << 15, 1 << 15},
+            // test override trumps test configured 2
+            {4, 7, 7},
+        };
+    }
+
+    @Parameterized.Parameter(0)
+    public int parallelism;
+
+    @Parameterized.Parameter(1)
+    public int maxParallelism;
+
+    @Parameterized.Parameter(2)
+    public int expectedMaxParallelism;
+
+    @Test
+    public void testMaxParallelismDefaulting() {
+        JobVertex jobVertex = createJobVertex(parallelism, maxParallelism);
+        JobGraph jobGraph = streamingJobGraph(jobVertex);
+        JobGraphConfigurationUtils.autoConfigureMaxParallelism(jobGraph);
+        Assert.assertEquals(expectedMaxParallelism, jobVertex.getMaxParallelism());
+    }
+
+    @Test
+    public void testMaxParallelismDefaultingReactiveMode() {
+        JobVertex jobVertex = createJobVertex(parallelism, maxParallelism);
+        JobGraph jobGraph = streamingJobGraph(jobVertex);
+        JobGraphConfigurationUtils.configureJobGraphForReactiveMode(jobGraph);
+        Assert.assertEquals(expectedMaxParallelism, jobVertex.getMaxParallelism());
+        Assert.assertEquals(expectedMaxParallelism, jobVertex.getParallelism());
+    }
+}

flink-runtime/src/test/java/org/apache/flink/runtime/jobgraph/JobGraphTestUtils.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.runtime.jobgraph;
 
+import org.apache.flink.runtime.jobgraph.tasks.AbstractInvokable;
 import org.apache.flink.runtime.testtasks.NoOpInvokable;
 
 import java.util.Arrays;
@@ -30,22 +31,45 @@ public class JobGraphTestUtils {
     }
 
     public static JobGraph singleNoOpJobGraph() {
-        JobVertex jobVertex = new JobVertex("jobVertex");
+        JobVertex jobVertex = createJobVertex("jobVertex", 1, JobVertex.MAX_PARALLELISM_DEFAULT);
         jobVertex.setInvokableClass(NoOpInvokable.class);
 
-        return JobGraphBuilder.newStreamingJobGraphBuilder().addJobVertex(jobVertex).build();
+        return streamingJobGraph(jobVertex);
     }
 
     public static JobGraph streamingJobGraph(JobVertex... jobVertices) {
-        return JobGraphBuilder.newStreamingJobGraphBuilder()
-                .addJobVertices(Arrays.asList(jobVertices))
-                .build();
+        JobGraph graph =
+                JobGraphBuilder.newStreamingJobGraphBuilder()
+                        .addJobVertices(Arrays.asList(jobVertices))
+                        .build();
+        JobGraphConfigurationUtils.autoConfigureMaxParallelism(graph);
+        return graph;
     }
 
     public static JobGraph batchJobGraph(JobVertex... jobVertices) {
-        return JobGraphBuilder.newBatchJobGraphBuilder()
-                .addJobVertices(Arrays.asList(jobVertices))
-                .build();
+        JobGraph graph =
+                JobGraphBuilder.newBatchJobGraphBuilder()
+                        .addJobVertices(Arrays.asList(jobVertices))
+                        .build();
+        JobGraphConfigurationUtils.autoConfigureMaxParallelism(graph);
+        return graph;
+    }
+
+    public static JobVertex createJobVertex(int parallelism, int preconfiguredMaxParallelism) {
+        return createJobVertex("testVertex", parallelism, preconfiguredMaxParallelism);
+    }
+
+    public static JobVertex createJobVertex(
+            String name, int parallelism, int preconfiguredMaxParallelism) {
+        JobVertex jobVertex = new JobVertex(name);
+        jobVertex.setInvokableClass(AbstractInvokable.class);
+        jobVertex.setParallelism(parallelism);
+
+        if (JobVertex.MAX_PARALLELISM_DEFAULT != preconfiguredMaxParallelism) {
+            jobVertex.setMaxParallelism(preconfiguredMaxParallelism);
+        }
+
+        return jobVertex;
     }
 
     private JobGraphTestUtils() {

flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/TestUtils.java
@@ -25,6 +25,7 @@ import org.apache.flink.runtime.checkpoint.OperatorSubtaskState;
 import org.apache.flink.runtime.checkpoint.metadata.CheckpointMetadata;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.jobgraph.JobGraphBuilder;
+import org.apache.flink.runtime.jobgraph.JobGraphConfigurationUtils;
 import org.apache.flink.runtime.jobgraph.JobVertex;
 import org.apache.flink.runtime.jobgraph.OperatorID;
 import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
@@ -109,11 +110,15 @@ public class TestUtils {
         final JobCheckpointingSettings checkpointingSettings =
                 new JobCheckpointingSettings(checkpoinCoordinatorConfiguration, null);
 
-        return JobGraphBuilder.newStreamingJobGraphBuilder()
-                .addJobVertices(Arrays.asList(jobVertices))
-                .setJobCheckpointingSettings(checkpointingSettings)
-                .setSavepointRestoreSettings(savepointRestoreSettings)
-                .build();
+        JobGraph graph =
+                JobGraphBuilder.newStreamingJobGraphBuilder()
+                        .addJobVertices(Arrays.asList(jobVertices))
+                        .setJobCheckpointingSettings(checkpointingSettings)
+                        .setSavepointRestoreSettings(savepointRestoreSettings)
+                        .build();
+
+        JobGraphConfigurationUtils.autoConfigureMaxParallelism(graph);
+        return graph;
     }
 
     private TestUtils() {